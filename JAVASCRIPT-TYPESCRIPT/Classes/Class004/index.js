71797b9c42e66785886011aec9334b98cdb1264b
maia18 <nelsonkaue05100@gmail.com>
2025-10-24 14:33:17 -0300

    updates

--- JAVASCRIPT-TYPESCRIPT/Classes/Class004/index.js
+++
@@ -1,4 +0,0 @@
-// Um comentário
-console.log("Hello, world!");
-console.log("Este trecho será exibido no console do navegador.");
-alert("Olá!!");
--- JAVASCRIPT-TYPESCRIPT/Exercises/Ex001/index.js
+++
@@ -1,1 +0,0 @@
- console.log("Meu nome é 'Kauê'. Estou aprendendo JavaScript às", 10 ,'da manhã');
